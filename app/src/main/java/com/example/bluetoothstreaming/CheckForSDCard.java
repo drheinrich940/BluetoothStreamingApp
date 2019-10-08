79fff8ce0165399e51b25c73e8795fedcb3d0829
Cyril <cyril.belmonte@hotmail.fr>
2019-10-08 16:18:15 +0200

    :construction: adding download, saving media

---
+++ app/src/main/java/com/example/bluetoothstreaming/CheckForSDCard.java
@@ -0,0 +1,16 @@
+package com.example.bluetoothstreaming;
+
+import android.os.Environment;
+
+public class CheckForSDCard {
+
+    //Method to Check If SD Card is mounted or not
+    public static boolean isSDCardPresent() {
+        if (Environment.getExternalStorageState().equals(
+
+                Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        return false;
+    }
+}
