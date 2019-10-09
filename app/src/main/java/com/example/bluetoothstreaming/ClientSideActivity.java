8b82289af296a2b8bcf6d4f90f51eb8ea78c7321
henri <drheinrich940@gmail.com>
2019-10-09 02:02:34 +0200

    :construction: :sparkles: list of bluetooth devices is working.

--- app/src/main/java/com/example/bluetoothstreaming/ClientSideActivity.java
+++ app/src/main/java/com/example/bluetoothstreaming/ClientSideActivity.java
@@ -7,59 +7,81 @@ import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
 
+//TODO : comments
+//TODO : refresh
+//TODO : call stream from server
 public class ClientSideActivity extends AppCompatActivity {
 
     ArrayList<BluetoothDevice> bleDevices = new ArrayList<>();
     BluetoothAdapter bleAdapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_side);
-        listAvaliableBluetoothPeriphericals();
-    }
-
-    public void listAvaliableBluetoothPeriphericals(){
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         bleAdapter = BluetoothAdapter.getDefaultAdapter();
+        Log.i("AAA", "AAAAA");
+
         if (!bleAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, 1);
         }
+
         Set<BluetoothDevice> bondedBleDevicesSet = bleAdapter.getBondedDevices();
-
         boolean discovery = bleAdapter.startDiscovery();
-
         if (bondedBleDevicesSet.size() > 0) {
             for (BluetoothDevice device : bondedBleDevicesSet) {
                 bleDevices.add(device);
             }
         }
+
+        listView = findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object listItem = listView.getItemAtPosition(position);
+                Log.i("onItemClick", "user selected following item :"+ listItem);
+            }
+
+        });
+
+        rechargeListView();
     }
+
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if(!bleDevices.contains(device))
+                if (!bleDevices.contains(device))
                     bleDevices.add(device);
             }
         }
     };
 
-    public void refreshList(View view) {
-        // TODO : Give ID to component list
-        ListView listView = findViewById(R.id.listView);
-        ArrayAdapter<BluetoothDevice> arrayAdapter = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_1, bleDevices);
-        listView.setAdapter(arrayAdapter);
+    public void rechargeList(View view) {
+        rechargeListView();
+    }
 
+    public void rechargeListView() {
+        ArrayList deviceList = new ArrayList(bleDevices);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, deviceList);
+        listView.setAdapter(arrayAdapter);
     }
 
     @Override
--- app/src/main/java/com/example/bluetoothstreaming/MainActivity.java
+++ app/src/main/java/com/example/bluetoothstreaming/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.bluetoothstreaming;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        Button clientButton = findViewById(R.id.clientButton);
+        clientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent clientIntent;
+                clientIntent = new Intent(MainActivity.this, ClientSideActivity.class);
+                startActivity(clientIntent);
+            }
+
+        });
+
 
 
     }
